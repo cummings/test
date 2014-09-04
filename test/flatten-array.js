7c0b36920d97f765501eeb068241434afdce211d
Poul Cummings <poul.cummings@gmail.com>
2014-09-04 10:11:21 +0100

    Completed tests

--- test/flatten-array.js
+++ test/flatten-array.js
@@ -1,8 +1,27 @@
 describe('flatten array', function () {
   it('should flatten an array', function () {
-    var arr = [1, 2, [1, 2, [3, 4, 5, [1]]], 2, [2]],
+    var tmpArray = [],
+        arr = [1, 2, [1, 2, [3, 4, 5, [1]]], 2, [2]],
         expected = [1, 1, 1, 2, 2, 2, 2, 3, 4, 5];
+
+   function go(arra) {
+      var self = arguments.callee;
+
+      arra.forEach(function (item) {
+        if(typeof item === 'object') {
+          self(item);
+        } else {
+          tmpArray.push(item);
+         }
+      });
+
+      return tmpArray;
+    }
+    arr =  go(arr).sort();
 
     expect(arr).toEqual(expected);
   });
 });
+
+
+
